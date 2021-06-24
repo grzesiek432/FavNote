2e8b985646d99885266c7cf2ffba19dbb8e4313d
Grzesiek <g.chojnacki0@gmail.com>
2021-06-24 20:15:55 +0200

    Add Heading.js, Paragraph.js in Basic form, Input.js with props

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { makeDecorator } from '@storybook/addons';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/theme/mainTheme';
+
+const withTheme = (storyFn) => {
+   return <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>;
+};
+
+const myThemed = makeDecorator({
+   name: 'my-theme',
+   wrapper: withTheme,
+});
+
+addDecorator(withTheme);
 export const parameters = {
    actions: { argTypesRegex: '^on[A-Z].*' },
    controls: {
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
 import { theme } from 'theme/mainTheme';
+import Input from 'components/atoms/Input/Input';
 
 const Root = () => (
    <div>
@@ -12,6 +13,7 @@ const Root = () => (
             <h1>Hello World</h1>
             <Button>Close/Save</Button>
             <Button secondary>Remove</Button>
+            <Input placeholder="search" search></Input>
          </>
       </ThemeProvider>
    </div>
