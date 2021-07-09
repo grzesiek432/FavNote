9a72b5421df455fda7c490ae427937c891d2b554
Grzesiek <g.chojnacki0@gmail.com>
2021-07-09 20:11:52 +0200

    Create a view single note in DetailsPage.js

--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,35 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 /* eslint-disable */
 import DetailsTemplate from 'templates/DetailsTemplate';
 import { routes } from 'routes';
 import PropTypes from 'prop-types';
+import withContext from 'hoc/withContext';
+import { connect } from 'react-redux';
+import { match } from 'esquery';
+import axios from 'axios';
 
-class DetailsPage extends React.Component {
+class DetailsPage extends Component {
+   state = {
+      activeItem: {
+         title: '',
+         content: '',
+         articleUrl: '',
+         twitterName: '',
+      },
+   };
+   componentDidMount() {
+      if (this.props.activeItem) {
+         const [activeItem] = this.props.activeItem;
+         this.setState({ activeItem });
+      } else {
+         const { id } = this.props.match.params;
+
+         axios
+            .get(`http://localhost:9000/api/note/${id}`)
+            .then(({ data }) => {
+               this.setState({ activeItem: data });
+            })
+            .catch(({ err }) => console.log(err));
+      }
+   }
    render() {
-      const dummyArticle = {
-         id: 1,
-         title: 'Wake me up when Vue ends',
-         content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-         twitterName: 'hello_roman',
-         articleUrl: 'https://youtube.com/helloroman',
-         created: '1 day',
-      };
-      const { match } = this.props;
+      const { activeItem } = this.state;
       return (
          <DetailsTemplate
-            title={dummyArticle.title}
-            content={dummyArticle.content}
-            created={dummyArticle.created}
-            articleUrl={dummyArticle.articleUrl}
-            twitterName={dummyArticle.twitterName}
+            title={activeItem.title}
+            content={activeItem.content}
+            articleUrl={activeItem.articleUrl}
+            twitterName={activeItem.twitterName}
          ></DetailsTemplate>
       );
    }
 }
 
+const mapStateToProps = (state, ownProps) => {
+   if (state[ownProps.pageContext]) {
+      return {
+         activeItem: state[ownProps.pageContext].filter(
+            (item) => item._id === ownProps.match.params.id,
+         ),
+      };
+   }
+};
+
 DetailsPage.propTypes = {
    match: PropTypes.object.isRequired,
 };
 
-export default DetailsPage;
+export default withContext(connect(mapStateToProps)(DetailsPage));
